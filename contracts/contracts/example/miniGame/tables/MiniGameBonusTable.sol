3c0d21829b2038305b25e00cb5c5172ebdd2df53
0x00 pluto <0x00pluto@gmail.com>
2023-06-01 18:58:21 +0800

    add table

--- contracts/contracts/example/miniGame/tables/MiniGameBonusTable.sol
+++ contracts/contracts/example/miniGame/tables/MiniGameBonusTable.sol
@@ -11,10 +11,18 @@ bytes32 constant _tableId = bytes32(
 bytes32 constant MiniGameBonusTableId = _tableId;
 
 library MiniGameBonusTable {
-    /** Set amount */
-    function set(address owner, uint256 amount) internal {
+    function entityKeys(
+        address owner
+    ) internal pure returns (bytes32[] memory) {
         bytes32[] memory _keyTuple = new bytes32[](1);
         _keyTuple[0] = bytes32(uint256(uint160((owner))));
+
+        return _keyTuple;
+    }
+
+    /** Set amount */
+    function set(address owner, uint256 amount) internal {
+        bytes32[] memory _keyTuple = entityKeys(owner);
 
         StoreDelegate.Store().setField(
             _tableId,
@@ -26,8 +34,7 @@ library MiniGameBonusTable {
 
     /** Get amount */
     function get(address owner) internal view returns (uint256 amount) {
-        bytes32[] memory _keyTuple = new bytes32[](1);
-        _keyTuple[0] = bytes32(uint256(uint160((owner))));
+        bytes32[] memory _keyTuple = entityKeys(owner);
 
         bytes memory _blob = StoreDelegate.Store().getField(
             _tableId,
@@ -37,5 +44,12 @@ library MiniGameBonusTable {
 
         if (_blob.length == 0) return 0;
         return abi.decode(_blob, (uint256));
+    }
+
+    /** Delete amount */
+    function deleteRecord(address owner) internal {
+        bytes32[] memory _keyTuple = entityKeys(owner);
+
+        StoreDelegate.Store().deleteRecord(_tableId, _keyTuple, 1);
     }
 }
