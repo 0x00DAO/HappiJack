4c1e34773b12f2f7d1c3028eab0a92eae2b44c05
0x00 pluto <0x00pluto@gmail.com>
2023-06-26 11:00:50 +0800

    support new network

--- subgraph/lotterygame/src/lottery-game-system.ts
+++ subgraph/lotterygame/src/lottery-game-system.ts
@@ -8,8 +8,8 @@ import {
   RoleGranted as RoleGrantedEvent,
   RoleRevoked as RoleRevokedEvent,
   Unpaused as UnpausedEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/LotteryGameSystem/LotteryGameSystem"
+  Upgraded as UpgradedEvent,
+} from '../generated/LotteryGameSystem/LotteryGameSystem';
 import {
   AdminChanged,
   BeaconUpgraded,
@@ -20,145 +20,145 @@ import {
   RoleGranted,
   RoleRevoked,
   Unpaused,
-  Upgraded
-} from "../generated/schema"
+  Upgraded,
+} from '../generated/schema';
 
 export function handleAdminChanged(event: AdminChangedEvent): void {
   let entity = new AdminChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+  );
+  entity.previousAdmin = event.params.previousAdmin;
+  entity.newAdmin = event.params.newAdmin;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
   let entity = new BeaconUpgraded(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
+  );
+  entity.beacon = event.params.beacon;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleInitialized(event: InitializedEvent): void {
   let entity = new Initialized(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
+  );
+  entity.version = event.params.version;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleLotteryGameCreated(event: LotteryGameCreatedEvent): void {
   let entity = new LotteryGameCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.lotteryGameId = event.params.lotteryGameId
-  entity.owner = event.params.owner
-  entity.startTime = event.params.startTime
-  entity.endTime = event.params.endTime
+  );
+  entity.lotteryGameId = event.params.lotteryGameId;
+  entity.owner = event.params.owner;
+  entity.startTime = event.params.startTime;
+  entity.endTime = event.params.endTime;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handlePaused(event: PausedEvent): void {
   let entity = new Paused(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
+  );
+  entity.account = event.params.account;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
   let entity = new RoleAdminChanged(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
+  );
+  entity.role = event.params.role;
+  entity.previousAdminRole = event.params.previousAdminRole;
+  entity.newAdminRole = event.params.newAdminRole;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleGranted(event: RoleGrantedEvent): void {
   let entity = new RoleGranted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.role = event.params.role;
+  entity.account = event.params.account;
+  entity.sender = event.params.sender;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleRoleRevoked(event: RoleRevokedEvent): void {
   let entity = new RoleRevoked(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
+  );
+  entity.role = event.params.role;
+  entity.account = event.params.account;
+  entity.sender = event.params.sender;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleUnpaused(event: UnpausedEvent): void {
   let entity = new Unpaused(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
+  );
+  entity.account = event.params.account;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleUpgraded(event: UpgradedEvent): void {
   let entity = new Upgraded(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
+  );
+  entity.implementation = event.params.implementation;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
