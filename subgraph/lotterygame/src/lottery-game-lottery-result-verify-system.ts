9a52ccc4e3a34ed56178ea852eaa148d4d6d3e57
0x00 pluto <0x00pluto@gmail.com>
2023-07-17 12:00:52 +0800

    add new contract

---
+++ subgraph/lotterygame/src/lottery-game-lottery-result-verify-system.ts
@@ -0,0 +1,164 @@
+import {
+  AdminChanged as AdminChangedEvent,
+  BeaconUpgraded as BeaconUpgradedEvent,
+  Initialized as InitializedEvent,
+  LotteryGameResultVerified as LotteryGameResultVerifiedEvent,
+  Paused as PausedEvent,
+  RoleAdminChanged as RoleAdminChangedEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent,
+  Unpaused as UnpausedEvent,
+  Upgraded as UpgradedEvent
+} from "../generated/LotteryGameLotteryResultVerifySystem/LotteryGameLotteryResultVerifySystem"
+import {
+  AdminChanged,
+  BeaconUpgraded,
+  Initialized,
+  LotteryGameResultVerified,
+  Paused,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  Unpaused,
+  Upgraded
+} from "../generated/schema"
+
+export function handleAdminChanged(event: AdminChangedEvent): void {
+  let entity = new AdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousAdmin = event.params.previousAdmin
+  entity.newAdmin = event.params.newAdmin
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
+  let entity = new BeaconUpgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.beacon = event.params.beacon
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleLotteryGameResultVerified(
+  event: LotteryGameResultVerifiedEvent
+): void {
+  let entity = new LotteryGameResultVerified(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.lotteryGameId = event.params.lotteryGameId
+  entity.luckyNumber = event.params.luckyNumber
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlePaused(event: PausedEvent): void {
+  let entity = new Paused(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
+  let entity = new RoleAdminChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.previousAdminRole = event.params.previousAdminRole
+  entity.newAdminRole = event.params.newAdminRole
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleGranted(event: RoleGrantedEvent): void {
+  let entity = new RoleGranted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRoleRevoked(event: RoleRevokedEvent): void {
+  let entity = new RoleRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.role = event.params.role
+  entity.account = event.params.account
+  entity.sender = event.params.sender
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUnpaused(event: UnpausedEvent): void {
+  let entity = new Unpaused(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.account = event.params.account
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUpgraded(event: UpgradedEvent): void {
+  let entity = new Upgraded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.implementation = event.params.implementation
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ subgraph/lotterygame/tests/lottery-game-lottery-result-verify-system-utils.ts
@@ -0,0 +1,199 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import {
+  AdminChanged,
+  BeaconUpgraded,
+  Initialized,
+  LotteryGameResultVerified,
+  Paused,
+  RoleAdminChanged,
+  RoleGranted,
+  RoleRevoked,
+  Unpaused,
+  Upgraded
+} from "../generated/LotteryGameLotteryResultVerifySystem/LotteryGameLotteryResultVerifySystem"
+
+export function createAdminChangedEvent(
+  previousAdmin: Address,
+  newAdmin: Address
+): AdminChanged {
+  let adminChangedEvent = changetype<AdminChanged>(newMockEvent())
+
+  adminChangedEvent.parameters = new Array()
+
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousAdmin",
+      ethereum.Value.fromAddress(previousAdmin)
+    )
+  )
+  adminChangedEvent.parameters.push(
+    new ethereum.EventParam("newAdmin", ethereum.Value.fromAddress(newAdmin))
+  )
+
+  return adminChangedEvent
+}
+
+export function createBeaconUpgradedEvent(beacon: Address): BeaconUpgraded {
+  let beaconUpgradedEvent = changetype<BeaconUpgraded>(newMockEvent())
+
+  beaconUpgradedEvent.parameters = new Array()
+
+  beaconUpgradedEvent.parameters.push(
+    new ethereum.EventParam("beacon", ethereum.Value.fromAddress(beacon))
+  )
+
+  return beaconUpgradedEvent
+}
+
+export function createInitializedEvent(version: i32): Initialized {
+  let initializedEvent = changetype<Initialized>(newMockEvent())
+
+  initializedEvent.parameters = new Array()
+
+  initializedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(version))
+    )
+  )
+
+  return initializedEvent
+}
+
+export function createLotteryGameResultVerifiedEvent(
+  lotteryGameId: BigInt,
+  luckyNumber: BigInt
+): LotteryGameResultVerified {
+  let lotteryGameResultVerifiedEvent = changetype<LotteryGameResultVerified>(
+    newMockEvent()
+  )
+
+  lotteryGameResultVerifiedEvent.parameters = new Array()
+
+  lotteryGameResultVerifiedEvent.parameters.push(
+    new ethereum.EventParam(
+      "lotteryGameId",
+      ethereum.Value.fromUnsignedBigInt(lotteryGameId)
+    )
+  )
+  lotteryGameResultVerifiedEvent.parameters.push(
+    new ethereum.EventParam(
+      "luckyNumber",
+      ethereum.Value.fromUnsignedBigInt(luckyNumber)
+    )
+  )
+
+  return lotteryGameResultVerifiedEvent
+}
+
+export function createPausedEvent(account: Address): Paused {
+  let pausedEvent = changetype<Paused>(newMockEvent())
+
+  pausedEvent.parameters = new Array()
+
+  pausedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+
+  return pausedEvent
+}
+
+export function createRoleAdminChangedEvent(
+  role: Bytes,
+  previousAdminRole: Bytes,
+  newAdminRole: Bytes
+): RoleAdminChanged {
+  let roleAdminChangedEvent = changetype<RoleAdminChanged>(newMockEvent())
+
+  roleAdminChangedEvent.parameters = new Array()
+
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousAdminRole",
+      ethereum.Value.fromFixedBytes(previousAdminRole)
+    )
+  )
+  roleAdminChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newAdminRole",
+      ethereum.Value.fromFixedBytes(newAdminRole)
+    )
+  )
+
+  return roleAdminChangedEvent
+}
+
+export function createRoleGrantedEvent(
+  role: Bytes,
+  account: Address,
+  sender: Address
+): RoleGranted {
+  let roleGrantedEvent = changetype<RoleGranted>(newMockEvent())
+
+  roleGrantedEvent.parameters = new Array()
+
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  roleGrantedEvent.parameters.push(
+    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
+  )
+
+  return roleGrantedEvent
+}
+
+export function createRoleRevokedEvent(
+  role: Bytes,
+  account: Address,
+  sender: Address
+): RoleRevoked {
+  let roleRevokedEvent = changetype<RoleRevoked>(newMockEvent())
+
+  roleRevokedEvent.parameters = new Array()
+
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("role", ethereum.Value.fromFixedBytes(role))
+  )
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  roleRevokedEvent.parameters.push(
+    new ethereum.EventParam("sender", ethereum.Value.fromAddress(sender))
+  )
+
+  return roleRevokedEvent
+}
+
+export function createUnpausedEvent(account: Address): Unpaused {
+  let unpausedEvent = changetype<Unpaused>(newMockEvent())
+
+  unpausedEvent.parameters = new Array()
+
+  unpausedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+
+  return unpausedEvent
+}
+
+export function createUpgradedEvent(implementation: Address): Upgraded {
+  let upgradedEvent = changetype<Upgraded>(newMockEvent())
+
+  upgradedEvent.parameters = new Array()
+
+  upgradedEvent.parameters.push(
+    new ethereum.EventParam(
+      "implementation",
+      ethereum.Value.fromAddress(implementation)
+    )
+  )
+
+  return upgradedEvent
+}
